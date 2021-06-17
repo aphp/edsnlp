d1bdb7c9cd735f1e5cc661c51196038fc34f790c
Basile Dura <basile.dura-ext@aphp.fr>
2021-06-17 12:58:23 +0200

    Correct setup

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
-from setuptools import setup
+import setuptools
 
-setup(name='nlptools',
-      version='1.0',
-      packages=['nlptools']
-      )
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nlptools",
+    version="0.0.1",
+    author="Data Science - DSI APHP",
+    author_email="basile.dura-ext@aphp.fr",
+    description="NLP tools for human consumption at AP-HP",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
+)

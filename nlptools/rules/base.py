3c1c8756ee3e800c783ed3f50ac8d53ad791251d
Charline JEAN <charline.jean-ext@aphp.fr>
2021-06-22 14:44:58 +0000

    Conjugation

--- nlptools/rules/base.py
+++ nlptools/rules/base.py
@@ -1,6 +1,8 @@
 from itertools import chain
 from typing import List, Optional, Tuple
 
+import mlconjug3
+import pandas as pd
 from spacy.tokens import Doc, Span
 
 if not Doc.has_extension('note_id'):
@@ -12,6 +14,7 @@ class BaseComponent(object):
     Base component that contains the logic for :
     - boundaries selections
     - match filtering
+    - verbs conjugation
     """
 
     split_on_punctuation = True
@@ -93,3 +96,45 @@ class BaseComponent(object):
         boundaries = [(start, end) for start, end in zip(starts[:-1], starts[1:])]
 
         return boundaries
+
+    @staticmethod
+    def _conjugate(verbs: List[str]) -> pd.DataFrame:
+        """
+        Create a list of conjugated verbs at all tenses from a list of infinitve verbs.
+
+        Parameters
+        ----------
+        verbs: List of infinitive verbs to conjugate
+
+        Returns
+        ----------
+        conjugated_verbs: Dataframe of conjugated verbs at all tenses
+        """
+
+        default_conjugator = mlconjug3.Conjugator(language='fr')
+
+        conjugated_verbs = pd.DataFrame(columns=["infinitif", "mode", "temps", "personne", "variant"])
+
+        for verb in verbs:
+            # Retrieve all conjugations
+            conjugated_verb = default_conjugator.conjugate(verb)
+            all_conjugated_forms = conjugated_verb.iterate()
+
+            # Instantiate a dataframe with the retrieved conjugations
+            df_verb = pd.DataFrame(all_conjugated_forms, columns=["mode", "temps", "personne", "variant"])
+            df_verb.insert(0, "infinitif", verb)
+
+            # Manipulate the infinitive form
+            df_verb.loc[df_verb["temps"] == "Infinitif Présent", "variant"] = verb
+            df_verb.loc[df_verb["temps"] == "Infinitif Présent", "personne"] = None
+
+            # Manipulate the present participle
+            part_present = df_verb.loc[df_verb["temps"] == "Participe Présent", "personne"]
+            df_verb.loc[df_verb["temps"] == "Participe Présent", "variant"] = part_present
+            df_verb.loc[df_verb["temps"] == "Participe Présent", "personne"] = None
+
+            conjugated_verbs = conjugated_verbs.append(df_verb)
+
+        conjugated_verbs = conjugated_verbs.dropna()
+
+        return conjugated_verbs

1d0f48c21cda0b01d7eecc205322e903cd608235
Ariel Cohen <ariel.cohen-ext@aphp.fr>
2021-06-24 13:48:05 +0000

    change install_requires to read the requirements.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+def get_lines(relative_path):
+    with open(relative_path) as f:
+        return f.readlines()
+
+INSTALL_REQUIRES = get_lines('requirements.txt')
+
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
@@ -13,13 +19,5 @@ setup(
     long_description_content_type="text/markdown",
     python_requires=">=3.6",
     packages=find_packages(),
-    install_requires=[
-        'loguru',
-        'mlconjug3',
-        'numpy',
-        'pandas',
-        'spacy',
-        'spaczz',
-        'unidecode',
-    ]
+    install_requires=INSTALL_REQUIRES
 )

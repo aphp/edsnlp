11f1b947ef5f56ef8ee775db0f52475ac8dc0897
Basile Dura <basile.dura-ext@aphp.fr>
2021-08-23 16:52:26 +0200

    Apply BLACK

--- edsnlp/rules/sections/terms.py
+++ edsnlp/rules/sections/terms.py
@@ -7,69 +7,111 @@ The section titles were reviewed by Gilles Chatellier, who gave meaningful insig
 See sections/section-dataset notebook for detail.
 """
 
-allergies = ['allergies']
+allergies = ["allergies"]
 
-antecedents = ['antecedents', 'antecedents medicaux et chirurgicaux',
-               'antecedents personnels', 'antecedents medicaux',
-               'antecedents chirurgicaux']
+antecedents = [
+    "antecedents",
+    "antecedents medicaux et chirurgicaux",
+    "antecedents personnels",
+    "antecedents medicaux",
+    "antecedents chirurgicaux",
+    "atcd",
+]
 
-antecedents_familiaux = ['antecedents familiaux']
+antecedents_familiaux = ["antecedents familiaux"]
 
-traitements_entree = ['attitude therapeutique initiale', "traitement a l'entree", 'traitement actuel',
-                      'traitement en cours', "traitements a l'entree"]
+traitements_entree = [
+    "attitude therapeutique initiale",
+    "traitement a l'entree",
+    "traitement actuel",
+    "traitement en cours",
+    "traitements a l'entree",
+]
 
-conclusion = ['au total', 'conclusion', 'conclusion de sortie', 'syntese medicale / conclusion',
-              'synthese', 'synthese medicale', 'synthese medicale/conclusion']
+conclusion = [
+    "au total",
+    "conclusion",
+    "conclusion de sortie",
+    "syntese medicale / conclusion",
+    "synthese",
+    "synthese medicale",
+    "synthese medicale/conclusion",
+    "conclusion medicale",
+]
 
 conclusion_entree = ["conclusion a l'entree"]
 
-habitus = ['contexte familial et social', 'habitus', 'mode de vie', 'mode de vie - scolarite',
-           'situation sociale, mode de vie']
+habitus = [
+    "contexte familial et social",
+    "habitus",
+    "mode de vie",
+    "mode de vie - scolarite",
+    "situation sociale, mode de vie",
+]
 
-correspondants = ['correspondants']
+correspondants = ["correspondants"]
 
-diagnostic = ['diagnostic retenu']
+diagnostic = ["diagnostic retenu"]
 
-donnees_biometriques_entree = ["donnees biometriques et parametres vitaux a l'entree",
-                               "parametres vitaux et donnees biometriques a l'entree"]
+donnees_biometriques_entree = [
+    "donnees biometriques et parametres vitaux a l'entree",
+    "parametres vitaux et donnees biometriques a l'entree",
+]
 
-examens = ['examen clinique', 'examen clinique', "examen clinique a l'entree"]
+examens = ["examen clinique", "examen clinique", "examen clinique a l'entree"]
 
-examens_complementaires = ['examen(s) complementaire(s)', 'examens complementaires',
-                           "examens complementaires a l'entree", 'examens complementaires realises pendant le sejour',
-                           'examens para-cliniques']
+examens_complementaires = [
+    "examen(s) complementaire(s)",
+    "examens complementaires",
+    "examens complementaires a l'entree",
+    "examens complementaires realises pendant le sejour",
+    "examens para-cliniques",
+]
 
-facteurs_de_risques = ['facteurs de risque', 'facteurs de risques']
+facteurs_de_risques = ["facteurs de risque", "facteurs de risques"]
 
-histoire_de_la_maladie = ['histoire de la maladie', 'histoire de la maladie - explorations',
-                          'histoire de la maladie actuelle', 'histoire du poids', 'histoire recente',
-                          'histoire recente de la maladie', 'rappel clinique', 'resume', 'resume clinique']
+histoire_de_la_maladie = [
+    "histoire de la maladie",
+    "histoire de la maladie - explorations",
+    "histoire de la maladie actuelle",
+    "histoire du poids",
+    "histoire recente",
+    "histoire recente de la maladie",
+    "rappel clinique",
+    "resume",
+    "resume clinique",
+]
 
-actes = ['intervention']
+actes = ["intervention"]
 
-motif = ['motif', "motif d'hospitalisation", "motif de l'hospitalisation", "motif medical"]
+motif = [
+    "motif",
+    "motif d'hospitalisation",
+    "motif de l'hospitalisation",
+    "motif medical",
+]
 
-prescriptions = ['prescriptions de sortie', 'prescriptions medicales de sortie']
+prescriptions = ["prescriptions de sortie", "prescriptions medicales de sortie"]
 
-traitements_sortie = ['traitement de sortie']
+traitements_sortie = ["traitement de sortie"]
 
 sections = {
-    'allergies': allergies,
-    'antécédents': antecedents,
-    'antécédents familiaux': antecedents_familiaux,
-    'traitements entrée': traitements_entree,
-    'conclusion': conclusion,
-    'conclusion entrée': conclusion_entree,
-    'habitus': habitus,
-    'correspondants': correspondants,
-    'diagnostic': diagnostic,
-    'données biométriques entrée': donnees_biometriques_entree,
-    'examens': examens,
-    'examens complémentaires': examens_complementaires,
-    'facteurs de risques': facteurs_de_risques,
-    'histoire de la maladie': histoire_de_la_maladie,
-    'actes': actes,
-    'motif': motif,
-    'prescriptions': prescriptions,
-    'traitements sortie': traitements_sortie,
+    "allergies": allergies,
+    "antécédents": antecedents,
+    "antécédents familiaux": antecedents_familiaux,
+    "traitements entrée": traitements_entree,
+    "conclusion": conclusion,
+    "conclusion entrée": conclusion_entree,
+    "habitus": habitus,
+    "correspondants": correspondants,
+    "diagnostic": diagnostic,
+    "données biométriques entrée": donnees_biometriques_entree,
+    "examens": examens,
+    "examens complémentaires": examens_complementaires,
+    "facteurs de risques": facteurs_de_risques,
+    "histoire de la maladie": histoire_de_la_maladie,
+    "actes": actes,
+    "motif": motif,
+    "prescriptions": prescriptions,
+    "traitements sortie": traitements_sortie,
 }

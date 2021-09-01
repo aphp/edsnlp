951b249a7a323f877098c98b0ca79184b44b1feb
Basile Dura <basile.dura-ext@aphp.fr>
2021-09-01 15:08:49 +0200

    Add examples creation utils

---
+++ edsnlp/utils/examples.py
@@ -0,0 +1,76 @@
+import re
+from typing import List, Union, Tuple
+
+from pydantic import BaseModel
+
+
+class Match(BaseModel):
+    start_char: int
+    end_char: int
+    text: str
+    modifiers: str
+
+
+class Modifier(BaseModel):
+    key: str
+    value: Union[int, float, bool, str]
+
+
+class Entity(BaseModel):
+    start_char: int
+    end_char: int
+    modifiers: List[Modifier]
+
+
+entity_pattern = re.compile(r"(<ent[^<>]*>[^<>]+</ent>)")
+text_pattern = re.compile(r"<ent.*>(.+)</ent>")
+modifiers_pattern = re.compile((r"<ent\s?(.*)>.+</ent>"))
+
+
+def find_matches(example: str) -> List[re.Match]:
+    return list(entity_pattern.finditer(example))
+
+
+def parse_match(match: re.Match) -> Match:
+
+    lexical_variant = match.group()
+    start_char = match.start()
+    end_char = match.end()
+
+    text = text_pattern.findall(lexical_variant)[0]
+    modifiers = modifiers_pattern.findall(lexical_variant)[0]
+
+    m = Match(start_char=start_char, end_char=end_char, text=text, modifiers=modifiers)
+
+    return m
+
+
+def parse_example(example: str) -> Tuple[str, List[Entity]]:
+
+    matches = [parse_match(match) for match in find_matches(example=example)]
+    text = ""
+    entities = []
+
+    cursor = 0
+
+    for match in matches:
+
+        text += example[cursor : match.start_char]
+        start_char = len(text)
+        text += match.text
+        end_char = len(text)
+        modifiers = [m.split("=") for m in match.modifiers.split()]
+
+        cursor = match.end_char
+
+        entity = Entity(
+            start_char=start_char,
+            end_char=end_char,
+            modifiers=[Modifier(key=k, value=v) for k, v in modifiers],
+        )
+
+        entities.append(entity)
+
+    text += example[cursor:]
+
+    return text, entities

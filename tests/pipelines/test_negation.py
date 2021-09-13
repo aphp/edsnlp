d1eb97003aa9709df5717b8539ab954704091311
Basile Dura <basile.dura-ext@aphp.fr>
2021-09-13 13:21:16 +0200

    Update changes for negation

--- tests/pipelines/test_negation.py
+++ tests/pipelines/test_negation.py
@@ -1,6 +1,11 @@
 from typing import List
 from edsnlp.utils.examples import parse_example
 
+from edsnlp.pipelines.negation import terms, Negation
+from edsnlp.pipelines import terminations
+
+from pytest import fixture
+
 negation_examples: List[str] = [
     "Le patient n'est pas <ent polarity_=NEG>malade</ent>.",
     "Aucun <ent polarity_=NEG>traitement</ent>.",
@@ -9,22 +14,78 @@ negation_examples: List[str] = [
     "il n'y a pas de <ent polarity_=NEG>métastases</ent>",
     "il n'y a pas d' <ent polarity_=NEG>métastases</ent>",
     "il n'y a pas d'<ent polarity_=NEG>métastases</ent>",
+    "il n'y a pas de <ent polarity_=NEG>métas,tases</ent>",
+    "<ent polarity_=NEG>métas,tases</ent> : non",
 ]
 
 
-def test_negation(nlp):
+@fixture
+def negation_factory(blank_nlp):
+
+    default_config = dict(
+        pseudo=terms.pseudo,
+        preceding=terms.preceding,
+        following=terms.following,
+        termination=terminations.termination,
+        verbs=terms.verbs,
+        fuzzy=False,
+        filter_matches=False,
+        attr="LOWER",
+        regex=None,
+        fuzzy_kwargs=None,
+    )
+
+    def factory(on_ents_only, **kwargs):
+
+        config = dict(**default_config)
+        config.update(kwargs)
+
+        return Negation(
+            nlp=blank_nlp,
+            on_ents_only=on_ents_only,
+            **config,
+        )
+
+    return factory
+
+
+def test_negation_no_ents_only(blank_nlp, negation_factory):
+
+    negation = negation_factory(on_ents_only=False)
 
     for example in negation_examples:
         text, entities = parse_example(example=example)
 
-        doc = nlp(text)
+        doc = blank_nlp(text)
+        doc.ents = [doc.char_span(ent.start_char, ent.end_char) for ent in entities]
 
-        for ent in entities:
+        doc = negation(doc)
 
-            span = doc.char_span(ent.start_char, ent.end_char)
+        for entity, ent in zip(entities, doc.ents):
 
-            for modifier in ent.modifiers:
+            for modifier in entity.modifiers:
 
-                assert all(
-                    [getattr(token._, modifier.key) == modifier.value for token in span]
+                assert (
+                    getattr(ent._, modifier.key) == modifier.value
+                ), f"{modifier.key} labels don't match."
+
+
+def test_negation_ents_only(blank_nlp, negation_factory):
+
+    negation = negation_factory(on_ents_only=True)
+
+    for example in negation_examples:
+        text, entities = parse_example(example=example)
+
+        doc = blank_nlp(text)
+        doc.ents = [doc.char_span(ent.start_char, ent.end_char) for ent in entities]
+
+        doc = negation(doc)
+
+        for entity, ent in zip(entities, doc.ents):
+
+            for modifier in entity.modifiers:
+
+                assert (
+                    getattr(ent._, modifier.key) == modifier.value
                 ), f"{modifier.key} labels don't match."

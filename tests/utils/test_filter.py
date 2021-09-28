9659c8922ce8b5cc07ad8274525c64514f63567e
Basile Dura <basile.dura-ext@aphp.fr>
2021-09-28 10:33:43 +0200

    Add tests for filter_span

---
+++ tests/utils/test_filter.py
@@ -0,0 +1,18 @@
+from edsnlp.utils.filter import filter_spans
+
+from spacy.tokens import Doc
+
+
+def test_filter_spans(doc: Doc):
+    spans = [
+        doc[0:3],
+        doc[0:4],
+        doc[1:2],
+        doc[0:2],
+        doc[0:3],
+    ]
+
+    filtered = filter_spans(spans)
+
+    assert len(filtered) == 1
+    assert len(filtered[0]) == 4

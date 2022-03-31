dd5e4e019a7e2f419e340dba4756dec54dcf6d99
Basile Dura <basile.dura@polytechnique.edu>
2022-03-31 14:23:13 +0200

    fix: wrong dict for months

--- edsnlp/pipelines/misc/dates/parser/absolute.py
+++ edsnlp/pipelines/misc/dates/parser/absolute.py
@@ -5,12 +5,12 @@ from edsnlp.pipelines.misc.dates.patterns.atomic import days, months
 from .factory import str2int, time2int_factory, time2int_fast_factory
 
 # Days
-day2int = time2int_factory(days.letter_days_dict)
 day2int_fast = time2int_fast_factory(days.letter_days_dict_simple)
+day2int = time2int_factory(days.letter_days_dict)
 
 # Months
-month2int_fast = time2int_factory(months.letter_months_dict_simple)
-month2int = time2int_factory(months.letter_month_pattern)
+month2int_fast = time2int_fast_factory(months.letter_months_dict_simple)
+month2int = time2int_factory(months.letter_months_dict)
 
 
 def parse_month(snippet: str) -> Optional[int]:

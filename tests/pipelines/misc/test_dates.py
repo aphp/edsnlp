920d80cf0df6544fc9d88a9ffd07fa0f9c8ef5f3
Basile Dura <basile.dura@polytechnique.edu>
2022-04-05 15:56:34 +0200

    tests: add dates testing

--- tests/pipelines/misc/test_dates.py
+++ tests/pipelines/misc/test_dates.py
@@ -1,135 +1,85 @@
-from datetime import datetime
-
-from pytest import fixture, raises
+import spacy
+from pytest import fixture
 from spacy.language import Language
-from spacy.tokens import Span
 
-from edsnlp.pipelines.misc.dates import Dates
-from edsnlp.pipelines.misc.dates.dates import apply_groupdict
-from edsnlp.pipelines.misc.dates.factory import DEFAULT_CONFIG
-from edsnlp.pipelines.misc.dates.parsing import day2int_fast, month2int_fast
+from edsnlp.utils.examples import parse_example
 
-text = (
-    "Le patient est venu hier (le 04/09/2021) pour un test PCR.\n"
-    "Il est cas contact depuis la semaine dernière, le 09/08 (2021-08-09)."
-)
-
-
-@fixture
-def dates(blank_nlp: Language):
-    return Dates(
-        blank_nlp,
-        **DEFAULT_CONFIG,
-    )
-
-
-def test_dates_on_ents_only(blank_nlp: Language):
-
-    blank_nlp.add_pipe("matcher", config=dict(terms={"contact": "contact"}))
-    blank_nlp.add_pipe("dates", config=dict(on_ents_only=True))
-
-    doc = blank_nlp(text)
-
-    assert len(doc.ents) == 1
-
-    assert len(doc.spans["dates"]) == 3
-
-
-def test_dates_component(blank_nlp: Language, dates: Dates):
-
-    doc = blank_nlp(text)
-
-    with raises(KeyError):
-        doc.spans["dates"]
-
-    doc = dates(doc)
-
-    d1, d2, d3, d4, d5 = doc.spans["dates"]
-
-    assert d1._.date == "TD-1"
-    assert d2._.date == "2021-09-04"
-    assert d3._.date == "TD-7"
-    assert d4._.date == "????-08-09"
-    assert d5._.date == "2021-08-09"
+examples = [
+    "Le patient est venu <ent direction=ago day=1>hier</ent>",
+    "le <ent day=4 month=9 year=2021>04/09/2021</ent>",
+    "Il est cas contact depuis <ent direction=ago week=1>la semaine dernière</ent>",
+    # "le <ent day=9 month=8>9/8</ent>",
+    "le <ent day=9 month=8>09/08</ent>",
+    "Le patient est venu le <ent day=4 month=8>4 août</ent>",
+    "Le patient est venu le <ent day=4 month=8 hour=11 minute=13>4 août à 11h13</ent>",
+    # "Le patient est venu en <ent year=2019>2019</ent> pour une consultation",
+    "Il est venu le <ent day=1 month=9>1er Septembre</ent> pour",
+    "Il est venu en <ent month=10 year=2020>octobre 2020</ent> pour...",
+    "Il est venu <ent direction=ago month=3>il y a trois mois</ent> pour...",
+    "Il lui était arrivé la même chose <ent direction=ago year=1>il y a un an</ent>.",
+    "Il est venu le <ent day=20 month=9 year=2001>20/09/2001</ent> pour...",
+    "Consultation du <ent direction=since day=3 month=7 year=19>03 07 19</ent>",
+    "En <ent month=11 year=2017>11/2017</ent> stabilité sur...",
+    "<ent direction=since month=3>depuis 3 mois</ent>",
+    "- <ent month=12 year=2004>Décembre 2004</ent> :",
+    "- <ent month=6 year=2005>Juin 2005</ent>:  ",
+    # "-<ent month=6 year=2005>Juin 2005</ent>:  ",  # issues with "fr" language
+    "<ent month=9 year=2017>sept 2017</ent> :",
+    (
+        "<ent direction=ago year=1>il y a 1 an</ent> "
+        "<ent direction=for month=1>pdt 1 mois</ent>"
+    ),
+    (
+        "Prélevé le : <ent day=22 month=4 year=2016>22/04/2016</ent> "
+        "\n78 rue du Général Leclerc"
+    ),
+    "Le <ent day=7 month=1>07/01</ent>.",
+    # "il est venu <ent day=4 month=8>cette année</ent>",
+    # "je vous écris <ent direction= day=1>ce jour</ent> à propos du patient",
+]
 
 
-def test_dates_with_base_date(blank_nlp: Language, dates: Dates):
-
-    doc = blank_nlp(text)
-    doc = dates(doc)
-
-    doc._.note_datetime = datetime(2020, 10, 10)
-
-    d1, d2, d3, d4, d5 = doc.spans["dates"]
-
-    assert d1._.date == "2020-10-09"
-    assert d2._.date == "2021-09-04"
-    assert d3._.date == "2020-10-03"
-    assert d4._.date == "2020-08-09"
-    assert d5._.date == "2021-08-09"
+@fixture(autouse=True)
+def add_date_pipeline(blank_nlp: Language):
+    blank_nlp.add_pipe("eds.dates")
 
 
-def test_absolute_dates_patterns(blank_nlp: Language, dates: Dates):
-
-    examples = [
-        ("Objet : Consultation du 03 07 19", "2019-07-03"),
-        ("Objet : Consultation du 03 juillet 19", "2019-07-03"),
-        ("Objet : Consultation du 3 juillet 19", "2019-07-03"),
-        ("Objet : Consultation du 03-07-19", "2019-07-03"),
-        ("Objet : Consultation du 03-07-1993", "1993-07-03"),
-        ("Objet : Consultation du 03.07.1993", "1993-07-03"),
-        ("Objet : Consultation du 1993-12-02", "1993-12-02"),
-        ("Objet : Consultation du 1993.12.02", "1993-12-02"),
-        ("en 09/17", "2017-09-01"),
-        ("13/07/2021 13:21", "2021-07-13"),
-        ("Objet : Compte-Rendu de Consultation du 03/10/2018", "2018-10-03"),
-        ("Objet : Compte-Rendu de Consultation du 03/10/2019 à 15h.", "2019-10-03"),
-    ]
-
-    for example, answer in examples:
-        doc = blank_nlp(example)
-        doc = dates(doc)
-
-        date = doc.spans["dates"][0]
-
-        assert date._.date == answer
-
-
-def test_patterns(blank_nlp: Language, dates: Dates):
-
-    examples = [
-        "Le patient est venu le 4 août",
-        "Le patient est venu le 4 août à 11h13",
-        "Le patient est venu en 2019 pour une consultation",
-        "Le patient est venu le 1er septembre pour une consultation",
-        "Le patient est venu le 1er Septembre pour une consultation",
-        "Le patient est venu en octobre 2020 pour une consultation",
-        "Le patient est venu il y a trois mois pour une consultation",
-        "Le patient est venu il y a un an pour une consultation",
-        "Il lui était arrivé la même chose il y a un an.",
-        "Le patient est venu le 20/09/2001 pour une consultation",
-        "Objet : Consultation du 03 07 19",
-        "En 11/2017 stabilité sur l'IRM médullaire des lésions",
-        "depuis 3 mois",
-        "- Décembre 2004 :",
-        "- Juin 2005:  ",
-        "-Avril 2011 :",
-        "sept 2017 :",
-        # "il y a 1 an pdt 1 mois",
-        "Prélevé le : 22/04/2016 \n78 rue du Général Leclerc",
-        "Le 07/01.",
-        "il est venu cette année",
-        "je vous écris ce jour à propos du patient",
-    ]
+def test_dates_component(blank_nlp: Language):
 
     for example in examples:
-        doc = blank_nlp(example)
-        doc = dates(doc)
+        text, entities = parse_example(example)
 
-        assert len(doc.spans["dates"]) == 1, doc.spans["dates"]
+        doc = blank_nlp(text)
+
+        assert len(doc.spans["dates"]) == len(entities)
+
+        for span, entity in zip(doc.spans["dates"], entities):
+            assert span.text == text[entity.start_char : entity.end_char]
+
+            date = {modifier.key: modifier.value for modifier in entity.modifiers}
+
+            assert span._.date.dict(exclude_none=True) == date
 
 
-def test_false_positives(blank_nlp: Language, dates: Dates):
+def test_periods(blank_nlp: Language):
+
+    period_examples = [
+        "en <ent>juin 2017 pendant trois semaines</ent>",
+        "du <ent>5 juin au 6 juillet</ent>",
+    ]
+
+    for example in period_examples:
+        text, entities = parse_example(example)
+
+        doc = blank_nlp(text)
+
+        assert len(doc.spans["periods"]) == len(entities)
+
+        for span, entity in zip(doc.spans["periods"], entities):
+            assert span.text == text[entity.start_char : entity.end_char]
+
+
+def test_false_positives(blank_nlp: Language):
 
     counter_examples = [
         "page 1/1",  # Often found in the form `1/1` only
@@ -150,127 +100,25 @@ def test_false_positives(blank_nlp: Language, dates: Dates):
 
     for example in counter_examples:
         doc = blank_nlp(example)
-        doc = dates(doc)
 
         assert len(doc.spans["dates"]) == 0
 
 
-def test_date_process(blank_nlp: Language, dates: Dates):
+def test_dates_on_ents_only():
 
-    examples = [
-        ("2019-11-21", ["full_date", "absolute"]),
-        ("22/10/2019", ["absolute"]),
-        ("04/11/2019", ["absolute"]),
-        ("22/10", ["no_year"]),
-        ("10/19", ["no_day"]),
-        ("10/11", ["no_year", "no_day"]),
-    ]
+    text = (
+        "Le patient est venu hier (le 04/09/2021) pour un test PCR.\n"
+        "Il est cas contact depuis la semaine dernière, le 09/08 (2021-08-09)."
+    )
 
-    for example, labels in examples:
-        doc = blank_nlp(example)
-        ds = list(dates.regex_matcher(doc, as_spans=True))
+    nlp = spacy.blank("eds")
 
-        assert [date.label_ for date in ds] == labels
+    nlp.add_pipe("eds.sentences")
+    nlp.add_pipe("eds.matcher", config=dict(terms={"contact": "contact"}))
+    nlp.add_pipe("eds.dates", config=dict(on_ents_only=True))
 
+    doc = nlp(text)
 
-def test_number_of_instances(blank_nlp):
-    blank_nlp.add_pipe("dates")
+    assert len(doc.ents) == 1
 
-    examples = [
-        (
-            (
-                "COMPTE RENDU D'HOSPITALISATION du 22/10/2019 au 05/11/2019\n"
-                "MOTIF D'HOSPITALISATION\n"
-                "Madame XX XX XX, née le 15/09/1973, "
-                "âgée de 46 ans, a été hospitalisée du 22/10/2019\n"
-                "au 04/11/2019 pour ischémie subaiguë gauche sur thrombose "
-                "d'un pontage ilio-femoral profond."
-            ),
-            5,
-        )
-    ]
-
-    for example, n in examples:
-        doc = blank_nlp(example)
-        assert len(doc.spans["dates"]) == n
-
-
-# def test_dates_with_time(blank_nlp):
-#     blank_nlp.add_pipe("dates")
-
-#     examples = [
-#         ("le trois septembre à 8h", "trois septembre à 8h"),
-#         ("22/10/2019 09:12", "22/10/2019 09:12"),
-#     ]
-
-#     for example, text in examples:
-#         doc = blank_nlp(example)
-#         d = doc.spans["dates"][0]
-#         assert d.text == text
-
-
-def test_parse_day_fast():
-    tests = [
-        ("5", 5),
-        ("trois", 3),
-        ("1", 1),
-        ("premier", 1),
-        ("dixsept", 17),
-        ("dix-sept", 17),
-        ("vingt-neuf", 29),
-        ("trente et un", 31),
-        ("trente-et-un", 31),
-        ("troi", None),
-        ("janvier", None),
-    ]
-
-    for test, answer in tests:
-        parsed = day2int_fast(test)
-        if answer is None:
-            assert parsed is None
-        else:
-            assert parsed == answer, test
-
-
-def test_parse_month_fast():
-    tests = [
-        ("janv", 1),
-        ("février", 2),
-        ("fevrier", 2),
-        ("jan", None),
-        ("neuf", None),
-    ]
-
-    for test, answer in tests:
-        parsed = month2int_fast(test)
-        if answer is None:
-            assert parsed is None
-        else:
-            assert parsed == answer
-
-
-def test_groupdict_parsing(blank_nlp, dates: Dates):
-
-    if not Span.has_extension("groupdict"):
-        Span.set_extension("groupdict", default=dict())
-
-    examples = [
-        ("Le 03/01/2012 à 9h15m32", dict(day=3, month=1, year=2012)),
-        # ("Le 03/01 2012 à 9h15m32", dict(day=3, month=1, year=2012)),
-        ("Le 3 janvier 2012 à 9h15m32", dict(day=3, month=1, year=2012)),
-        ("Le trois janvier 2012 à 9h15m32", dict(day=3, month=1, year=2012)),
-        ("Le trente et un décembre 2012 à 9h15m32", dict(day=31, month=12, year=2012)),
-    ]
-
-    for text, d in examples:
-        doc = blank_nlp(text)
-        spans = apply_groupdict(
-            dates.regex_matcher(
-                doc,
-                as_spans=True,
-                return_groupdict=True,
-            )
-        )
-        span = list(spans)[0]
-
-        assert span._.groupdict == d, text
+    assert len(doc.spans["dates"]) == 3
--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,9 +1,17 @@
+import shutil
 from itertools import chain
 from pathlib import Path
 
 import pytest
 
 from edsnlp.utils.blocs import check_md_file
+
+
+@pytest.fixture(autouse=True, scope="module")
+def brat_folder():
+    yield
+    shutil.rmtree("path/to/brat")
+
 
 files = chain(
     Path("./").glob("*.md"),

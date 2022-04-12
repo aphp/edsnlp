b5b420e5219b5af18dc4c6bd8f92553934d24cd2
Basile Dura <basile.dura-ext@aphp.fr>
2022-04-12 17:33:19 +0200

    test: add TNM test

---
+++ tests/pipelines/ner/test_tnm.py
@@ -0,0 +1,21 @@
+from edsnlp.utils.examples import parse_example
+
+examples = [
+    "TNM: <ent norm=aTxN1M0>aTxN1M0</ent>",
+    "TNM: <ent norm=pTxN1M0>p Tx N1M 0</ent>",
+]
+
+
+def test_scores(blank_nlp):
+
+    blank_nlp.add_pipe("eds.TNM")
+
+    for example in examples:
+
+        text, entities = parse_example(example=example)
+
+        doc = blank_nlp(text)
+
+        for entity, ent in zip(entities, doc.ents):
+            norm = entity.modifiers[0].value
+            assert norm == ent._.value.norm()

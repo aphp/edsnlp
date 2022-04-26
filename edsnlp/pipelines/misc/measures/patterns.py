9dd900b88338929ce18670eec107ec8ce01280a9
konsfra <francois.konschelle@chu-bordeaux.fr>
2022-04-26 15:23:27 +0200

    add energy and pressure and temperature units

--- edsnlp/pipelines/misc/measures/patterns.py
+++ edsnlp/pipelines/misc/measures/patterns.py
@@ -63,7 +63,7 @@ class Weight(SimpleMeasure):
     - kg
     """
 
-    COMPOSITE = CompositeWeight
+    COMPOSITE = None
     UNITS = {
         "mg": {"prefix": "mill?ig", "abbr": "mg", "value": 1},
         "cg": {"prefix": "centig", "abbr": "cg", "value": 10},
@@ -95,7 +95,7 @@ class Angle(SimpleMeasure):
     COMPOSITE = None
     UNITS = {
         "h": {"prefix": "heur", "abbr": "h", "value": 1},
-        "deg": {"prefix": "deg", "abbr": "°", "value": 1}, 
+        "degree": {"prefix": "deg", "abbr": "°", "value": 1}, 
     }
 
     @classmethod
@@ -107,6 +107,30 @@ class Angle(SimpleMeasure):
         return cls(result, unit)
 
     h = property(make_simple_getter("h"))
+    degree = property(make_simple_getter("degree"))
+
+@spacy.registry.misc("eds.measures.temperature")
+class Temperature(SimpleMeasure):
+    """
+    Temperature measure. Supports the following units:
+    - CelsiusDegree
+
+    Note that temperature and angle can be expressed usint the same symbol °
+    But Celsius-degree is usually given in decimal units while angular degree is
+    usually given in degree and minutes. 
+    """
+
+    COMPOSITE = None
+    UNITS = {
+        "CelsiusDegree": {"prefix": "deg", "abbr": "°", "value": 1}, 
+    }
+
+    @classmethod
+    def parse(cls, int_part, dec_part, unit, infix=False):
+        result = float("{}.{}".format(int_part, dec_part))
+        return cls(result, unit)
+
+    CelsiusDegree = property(make_simple_getter("CelsiusDegree"))
 
 
 @spacy.registry.misc("eds.measures.volume")
@@ -121,7 +145,7 @@ class Volume(SimpleMeasure):
     - goutte
     """
 
-    COMPOSITE = CompositeSize
+    COMPOSITE = None
     UNITS = {
         "mL": {"prefix": "mill?ilitre", "abbr": "ml", "value": 1},
         "cL": {"prefix": "centilitre", "abbr": "cl", "value": 10},
@@ -142,3 +166,77 @@ class Volume(SimpleMeasure):
     L = property(make_simple_getter("L"))
     cc = property(make_simple_getter("cc"))
     goutte = property(make_simple_getter("goutte"))
+
+@spacy.registry.misc("eds.measures.energy")
+class Energy(SimpleMeasure):
+    """
+    Energy measure. Supports the following units:
+    - J
+    - kJ
+    - MJ
+    - cal
+    - kcal
+    """
+
+    COMPOSITE = None
+    UNITS = {
+        "J": {"prefix": "joule", "abbr": "j", "value": 1},
+        "kJ": {"prefix": "kilojou", "abbr": "kj", "value": 0.001},
+        "MJ": {"prefix": "megajou", "abbr": "mj", "value": 0.000001},
+        "cal": {"prefix": "calorie", "abbr": "cal", "value": 4.184},
+        "kcal":{"prefix": "kilocal",  "abbr": "kcal", "value": 0.004184}, 
+    }
+
+    @classmethod
+    def parse(cls, int_part, dec_part, unit, infix=False):
+        result = float("{}.{}".format(int_part, dec_part))
+        return cls(result, unit)
+
+    J = property(make_simple_getter("J"))
+    kJ = property(make_simple_getter("kJ"))
+    MJ = property(make_simple_getter("MJ"))
+    cal = property(make_simple_getter("cal"))
+    kcal = property(make_simple_getter("kcal"))
+
+@spacy.registry.misc("eds.measures.pressure")
+class Pressure(SimpleMeasure):
+    """
+    Pressure measure. Supports the following units:
+    - Pa
+    - kPa
+    - hPa
+    - atm
+    - mmHg
+    - Torr
+    - bar
+    - mbar
+    - psi
+    """
+
+    COMPOSITE = None
+    UNITS = {
+        "Pa": {"prefix": "pascal", "abbr": "pa", "value": 1},
+        "kPa": {"prefix": "kilopascal", "abbr": "kpa", "value": 1000},
+        "hPa": {"prefix": "hectopascal", "abbr": "hpa", "value": 100},
+        "atm": {"prefix": "atmosphere", "abbr": "atm", "value": 101325},
+        "mmHg": {"prefix": "milli?mercure",  "abbr": "mmhg", "value": 133.3224}, 
+        "Torr": {"prefix": "torr",  "abbr": "torr", "value": 133.3224}, 
+        "bar": {"prefix": "bar",  "abbr": "bar", "value": 100000}, 
+        "mbar": {"prefix": "millibar",  "abbr": "mbar", "value": 100}, 
+        "psi": {"prefix": "psi",  "abbr": "psi", "value": 6894.757}, 
+    }
+
+    @classmethod
+    def parse(cls, int_part, dec_part, unit, infix=False):
+        result = float("{}.{}".format(int_part, dec_part))
+        return cls(result, unit)
+
+    Pa = property(make_simple_getter("Pa"))
+    kPa = property(make_simple_getter("kPa"))
+    hPa = property(make_simple_getter("hPa"))
+    atm = property(make_simple_getter("atm"))
+    mmHg = property(make_simple_getter("mmHg"))
+    Torr = property(make_simple_getter("Torr"))
+    bar = property(make_simple_getter("bar"))
+    mbar = property(make_simple_getter("mbar"))
+    psi = property(make_simple_getter("psi"))

6234636001bce19e272a88c90974559544d9b669
Ariel Cohen <ac.arielcohen@gmail.com>
2022-07-12 11:46:42 +0200

    pre commit

--- edsnlp/pipelines/ner/scores/tnm/models.py
+++ edsnlp/pipelines/ner/scores/tnm/models.py
@@ -1,7 +1,16 @@
 from enum import Enum
-from typing import Optional, Union
+from typing import TYPE_CHECKING, Optional, Union
 
 from pydantic import BaseModel, validator
+
+if TYPE_CHECKING:
+    from pydantic.typing import (
+        AbstractSetIntStr,
+        DictStrAny,
+        MappingIntStrAny,
+    )
+
+import warnings
 
 
 class TnmEnum(Enum):
@@ -75,3 +84,59 @@ class TNM(BaseModel):
             norm.append(f" ({self.version.upper()} {self.version_year})")
 
         return "".join(norm)
+
+    def dict(
+        self,
+        *,
+        include: Union["AbstractSetIntStr", "MappingIntStrAny"] = None,
+        exclude: Union["AbstractSetIntStr", "MappingIntStrAny"] = None,
+        by_alias: bool = False,
+        skip_defaults: bool = None,
+        exclude_unset: bool = False,
+        exclude_defaults: bool = False,
+        exclude_none: bool = False,
+    ) -> "DictStrAny":
+        """
+        Generate a dictionary representation of the model,
+        optionally specifying which fields to include or exclude.
+
+        """
+        if skip_defaults is not None:
+            warnings.warn(
+                f"""{self.__class__.__name__}.dict(): "skip_defaults"
+                is deprecated and replaced by "exclude_unset" """,
+                DeprecationWarning,
+            )
+            exclude_unset = skip_defaults
+
+        d = dict(
+            self._iter(
+                to_dict=True,
+                by_alias=by_alias,
+                include=include,
+                exclude=exclude,
+                exclude_unset=exclude_unset,
+                exclude_defaults=exclude_defaults,
+                exclude_none=exclude_none,
+            )
+        )
+        set_keys = set(d.keys())
+        for k in set_keys.intersection({"modifier", "tumour", "node", "metastasis"}):
+            v = d[k]
+            if isinstance(v, int):
+                d[k] = {
+                    f"{k}_string": None,
+                    f"{k}_int": v,
+                }
+            elif isinstance(v, TnmEnum):
+                d[k] = {
+                    f"{k}_string": v.value,
+                    f"{k}_int": None,
+                }
+            else:
+                d[k] = {
+                    f"{k}_string": None,
+                    f"{k}_int": None,
+                }
+
+        return d

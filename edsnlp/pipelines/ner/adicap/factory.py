84d0e8a1c968b54a6394924d1d8dc4991ac38e7f
Ariel Cohen <ac.arielcohen@gmail.com>
2022-08-31 18:40:56 +0200

    update with contextual matcher

--- edsnlp/pipelines/ner/adicap/factory.py
+++ edsnlp/pipelines/ner/adicap/factory.py
@@ -4,10 +4,7 @@ from spacy.language import Language
 
 from .adicap import Adicap
 
-DEFAULT_CONFIG = dict(
-    pattern=None,
-    attr="TEXT",
-)
+DEFAULT_CONFIG = dict(pattern=None, prefix=None, attr="TEXT", window=-500)
 
 
 @Language.factory(
@@ -19,10 +16,8 @@ def create_component(
     nlp: Language,
     name: str,
     pattern: Optional[Union[List[str], str]],
+    prefix: Optional[Union[List[str], str]],
+    window: int,
     attr: str,
 ):
-    return Adicap(
-        nlp,
-        pattern=pattern,
-        attr=attr,
-    )
+    return Adicap(nlp, pattern=pattern, attr=attr, prefix=prefix, window=window)
--- edsnlp/pipelines/ner/adicap/patterns.py
+++ edsnlp/pipelines/ner/adicap/patterns.py
@@ -1,10 +1,7 @@
 """
+Source :
 https://esante.gouv.fr/sites/default/files/media_entity/documents/cgts_sem_adicap_fiche-detaillee.pdf
 """
-
-# adicap_pattern = r"(?i)(?<=(?:(?:codification|adicap)[-\s:]*)
-# ([A-Z]{4}(\d{4}|[A-Z][0-9A-Z][A-Z][0-9]|[0-9A-Z][0-9][09A-Z][0-9]|0[A-Z][0-9]{2})+\s?,\s?)*)
-# ([A-Z]{4}(\d{4}|[A-Z][0-9A-Z][A-Z][0-9]|[0-9A-Z][0-9][09A-Z][0-9]|0[A-Z][0-9]{2})+)"
 
 
 d1_4 = r"[A-Z]{4}"
@@ -13,27 +10,8 @@ d5_8_v2 = r"\d{4}|[A-Z][0-9A-Z][A-Z][0-9]"
 d5_8_v3 = r"[0-9A-Z][0-9][09A-Z][0-9]"
 d5_8_v4 = r"0[A-Z][0-9]{2}"
 
-# code = (
-#     r"("
-#     + d1_4
-#     + r"("
-#     + d5_8_v1
-#     + r"|"
-#     + d5_8_v2
-#     + r"|"
-#     + d5_8_v3
-#     + r"|"
-#     + d5_8_v4
-#     + r")+"
-# )
 
-# adicap_pattern = r"(?i)(?<=(?:(?:codification|adicap)[-\s:]*)"
-# adicap_pattern += code
-# adicap_pattern += r"\s?,\s?)*)"
-# adicap_pattern += code
-# adicap_pattern += r")"
-
-code_adicap = r"(?i)(codification|adicap)"
+adicap_prefix = r"(?i)(codification|adicap)"
 base_code = (
     d1_4 + r"(" + d5_8_v1 + r"|" + d5_8_v2 + r"|" + d5_8_v3 + r"|" + d5_8_v4 + r")"
 )

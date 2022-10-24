b424e404ad4e89de798c6fba82dec396935bee26
Perceval Wajsburt <perceval.wajsburt@sorbonne-universite.fr>
2022-10-24 12:33:19 +0000

    fix: fix filter_spans for strictly nested entities

--- tests/utils/test_filter.py
+++ tests/utils/test_filter.py
@@ -18,6 +18,18 @@ def test_filter_spans(doc: Doc):
     assert len(filtered[0]) == 4
 
 
+def test_filter_spans_strict_nesting(doc: Doc):
+    spans = [
+        doc[0:5],
+        doc[1:4],
+    ]
+
+    filtered = filter_spans(spans)
+
+    assert len(filtered) == 1
+    assert len(filtered[0]) == 5
+
+
 def test_label_to_remove(doc: Doc):
 
     spans = [

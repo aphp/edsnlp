08e4d8d6a96c3a6acb48d6f5f8e9af442d648440
Ariel Cohen <ariel.cohen-ext@aphp.fr>
2023-01-19 12:04:07 +0100

    replace parameter

--- edsnlp/pipelines/misc/tables/tables.py
+++ edsnlp/pipelines/misc/tables/tables.py
@@ -88,6 +88,6 @@ class Tables(GenericMatcher):
             sep=self.sep,
             engine="python",
             header=None,
-            error_bad_lines=False,
+            on_bad_lines="skip",
         )
         return parsed

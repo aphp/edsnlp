97dd27172a349c8d8bdb2818a6405e4d4b0cffd5
Perceval Wajsbürt <perceval.wajsburt-ext@aphp.fr>
2023-09-13 01:11:52 +0200

    chore: update entry points

--- edsnlp/pipelines/factories.py
+++ edsnlp/pipelines/factories.py
@@ -1,4 +1,5 @@
-# flake8: noqa: F811
+# ruff: noqa: E501
+# flake8: noqa: F401, F811
 from .core.context.factory import create_component as context
 from .core.contextual_matcher.factory import create_component as contextual_matcher
 from .core.endlines.factory import create_component as endlines
@@ -8,6 +9,7 @@ from .core.normalizer.factory import create_component as normalizer
 from .core.normalizer.lowercase.factory import remove_lowercase
 from .core.normalizer.pollution.factory import create_component as pollution
 from .core.normalizer.quotes.factory import create_component as quotes
+from .core.normalizer.spaces.factory import create_component as spaces
 from .core.sentences.factory import create_component as sentences
 from .core.terminology.factory import create_component as terminology
 from .misc.consultation_dates.factory import create_component as consultation_dates
@@ -21,18 +23,18 @@ from .ner.behaviors.alcohol.factory import create_component as alcohol
 from .ner.behaviors.tobacco.factory import create_component as tobacco
 from .ner.cim10.factory import create_component as cim10
 from .ner.covid.factory import create_component as covid
-from .ner.disorders.AIDS.factory import create_component as AIDS
+from .ner.disorders.aids.factory import create_component as aids
 from .ner.disorders.cerebrovascular_accident.factory import (
     create_component as cerebrovascular_accident,
 )
-from .ner.disorders.CKD.factory import create_component as CKD
+from .ner.disorders.ckd.factory import create_component as ckd
 from .ner.disorders.congestive_heart_failure.factory import (
     create_component as congestive_heart_failure,
 )
 from .ner.disorders.connective_tissue_disease.factory import (
     create_component as connective_tissue_disease,
 )
-from .ner.disorders.COPD.factory import create_component as COPD
+from .ner.disorders.copd.factory import create_component as copd
 from .ner.disorders.dementia.factory import create_component as dementia
 from .ner.disorders.diabetes.factory import create_component as diabetes
 from .ner.disorders.hemiplegia.factory import create_component as hemiplegia
@@ -51,12 +53,13 @@ from .ner.disorders.peripheral_vascular_disease.factory import (
 from .ner.disorders.solid_tumor.factory import create_component as solid_tumor
 from .ner.drugs.factory import create_component as drugs
 from .ner.scores.charlson.factory import create_component as charlson
+from .ner.scores.elston_ellis.factory import create_component as elston_ellis
 from .ner.scores.emergency.ccmu.factory import create_component as ccmu
 from .ner.scores.emergency.gemsa.factory import create_component as gemsa
 from .ner.scores.emergency.priority.factory import create_component as priority
 from .ner.scores.factory import create_component as score
 from .ner.scores.sofa.factory import create_component as sofa
-from .ner.scores.tnm.factory import create_component as tnm
+from .ner.tnm.factory import create_component as tnm
 from .ner.umls.factory import create_component as umls
 from .qualifiers.family.factory import create_component as family
 from .qualifiers.history.factory import create_component as history

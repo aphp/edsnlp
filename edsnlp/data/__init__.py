644a2b7bed1f4935b34a29183db07528969d02b0
Perceval Wajsbürt <perceval.wajsburt@aphp.fr>
2023-11-20 00:48:36 +0100

    feat: lazy module loading (quick import + easy handling optional deps e.g. torch)

--- edsnlp/data/__init__.py
+++ edsnlp/data/__init__.py
@@ -1,7 +1,13 @@
-from .base import from_iterable, to_iterable
-from .standoff import read_standoff, write_standoff
-from .brat import read_brat, write_brat
-from .json import read_json, write_json
-from .spark import from_spark, to_spark
-from .pandas import from_pandas, to_pandas
-from .converters import get_dict2doc_converter, get_doc2dict_converter
+from typing import TYPE_CHECKING
+from edsnlp.utils.lazy_module import lazify
+
+lazify()
+
+if TYPE_CHECKING:
+    from .base import from_iterable, to_iterable
+    from .standoff import read_standoff, write_standoff
+    from .brat import read_brat, write_brat
+    from .json import read_json, write_json
+    from .spark import from_spark, to_spark
+    from .pandas import from_pandas, to_pandas
+    from .converters import get_dict2doc_converter, get_doc2dict_converter

6679f2472c70a7c7c880c7c261cc75713fd6b454
Perceval Wajsbürt <perceval.wajsburt@aphp.fr>
2023-11-20 19:14:23 +0100

    feat: allow to keep/skip none values during training with keep_none parameter in span qualifier

--- tests/pipelines/trainable/test_span_qualifier.py
+++ tests/pipelines/trainable/test_span_qualifier.py
@@ -46,8 +46,8 @@ def gold():
     return [doc1, doc2]
 
 
-@pytest.mark.parametrize("with_label_constraints", [True, False])
-def test_span_qualifier(gold, with_label_constraints):
+@pytest.mark.parametrize("with_constraints_and_not_none", [True, False])
+def test_span_qualifier(gold, with_constraints_and_not_none):
     import torch
 
     nlp = edsnlp.blank("eds")
@@ -70,17 +70,18 @@ def test_span_qualifier(gold, with_label_constraints):
                 "span_getter": ["ents", "sc"],
             },
             "qualifiers": {"_.event_type": ("event",), "_.test_negated": True}
-            if with_label_constraints
+            if with_constraints_and_not_none
             else ["_.test_negated", "_.event_type"],
+            "keep_none": not with_constraints_and_not_none,
         },
     )
     qlf = nlp.get_pipe("qualifier")
     qlf.post_init(gold, set())
-    if with_label_constraints:
+    if with_constraints_and_not_none:
         assert qlf.qualifiers == {"_.event_type": ["event"], "_.test_negated": True}
     else:
         assert qlf.qualifiers == {"_.event_type": True, "_.test_negated": True}
-    if with_label_constraints:
+    if with_constraints_and_not_none:
         qlf.classifier.bias.data += torch.tensor([0, 1000, 1000, 0])
         assert qlf.bindings == [
             ("_.test_negated", False),
@@ -103,7 +104,7 @@ def test_span_qualifier(gold, with_label_constraints):
         for ent in get_spans(doc, qlf.embedding.span_getter):
             assert ent._.test_negated is True
             if ent.label_ == "event":
-                if with_label_constraints is not None:
+                if with_constraints_and_not_none is not None:
                     assert ent._.event_type == "start"
                 else:
                     assert ent._.event_type is None

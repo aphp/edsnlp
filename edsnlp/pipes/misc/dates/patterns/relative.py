cd57757ef129636b6df904ccce8aaf03b5c3f8ff
Perceval Wajsbürt <perceval.wajsburt@aphp.fr>
2023-12-01 02:57:09 +0100

    refacto: rename pipelines to pipes

---
+++ edsnlp/pipes/misc/dates/patterns/relative.py
@@ -0,0 +1,54 @@
+from edsnlp.utils.regex import make_pattern
+
+from .atomic import directions, numbers, units
+from .atomic.modes import mode_pattern
+from .current import current_pattern
+
+
+def make_specific_pattern(mode: str = "forward"):
+    if mode == "forward":
+        p = directions.preceding_direction_pattern
+        p += r"\s+"
+        p += numbers.number_pattern
+        p += r"\s*"
+        p += units.unit_pattern
+    elif mode == "backward":
+        p = numbers.number_pattern
+        p += r"\s*"
+        p += units.unit_pattern
+        p += r"\s+"
+        p += directions.following_direction_pattern
+    else:
+        p = directions.preceding_direction_pattern
+        p += r"\s+"
+        p += numbers.number_pattern
+        p += r"\s*"
+        p += units.unit_pattern
+        p += r"\s+"
+        p += directions.following_direction_pattern
+
+    return p
+
+
+specific = {
+    "minus1": (r"hier", dict(direction="past", day=1)),
+    "minus2": (r"avant[-\s]hier", dict(direction="past", day=2)),
+    "plus1": (r"demain", dict(direction="future", day=1)),
+    "plus2": (r"après[-\s]demain", dict(direction="future", day=2)),
+}
+
+specific_pattern = make_pattern(
+    [f"(?P<specific_{k}>{p})" for k, (p, _) in specific.items()],
+)
+
+specific_dict = {k: v for k, (_, v) in specific.items()}
+
+relative_pattern = [
+    make_specific_pattern(mode="forward"),
+    make_specific_pattern(mode="backward"),
+    make_specific_pattern(mode="all"),
+    specific_pattern,
+    current_pattern,
+]
+
+relative_pattern = [r"(?<=" + mode_pattern + r".{,3})?" + p for p in relative_pattern]
---
+++ edsnlp/pipes/ner/scores/sofa/patterns.py
@@ -0,0 +1,42 @@
+from typing import Union
+
+import spacy
+
+regex = [r"\bsofa\b"]
+
+digits = r"[^\d]*(\d*)"
+
+value_extract = [
+    dict(
+        name="method_max",
+        regex=r"(max)",
+        reduce_mode="keep_first",
+    ),
+    dict(
+        name="method_24h",
+        regex=r"(24h)",
+        reduce_mode="keep_first",
+    ),
+    dict(
+        name="method_adm",
+        regex=r"(admission)",
+        reduce_mode="keep_first",
+    ),
+    dict(
+        name="value",
+        regex=r"^.*?[\n\W]*?(\d+)(?![h0-9])",
+    ),
+]
+
+score_normalization_str = "score_normalization.sofa"
+
+
+@spacy.registry.misc(score_normalization_str)
+def score_normalization(extracted_score: Union[str, None]):
+    """
+    Sofa score normalization.
+    If available, returns the integer value of the SOFA score.
+    """
+    score_range = list(range(0, 30))
+    if (extracted_score is not None) and (int(extracted_score) in score_range):
+        return int(extracted_score)

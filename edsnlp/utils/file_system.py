7e9a9036b9bb4f33f7c17e78a01660d2ae3e07c5
Perceval Wajsbürt <perceval.wajsburt@aphp.fr>
2024-03-14 03:28:07 +0100

    fix: patch and wrap fsspec for python 3.7

--- edsnlp/utils/file_system.py
+++ edsnlp/utils/file_system.py
@@ -1,12 +1,41 @@
+import fnmatch
 import os
 from pathlib import Path
 from typing import Optional, Tuple, Union
 
 import pyarrow.fs
 from fsspec import AbstractFileSystem
+from fsspec import __version__ as fsspec_version
 from fsspec.implementations.arrow import ArrowFSWrapper
 
 FileSystem = Union[AbstractFileSystem, pyarrow.fs.FileSystem]
+
+if fsspec_version < "2023.3.0":
+    # Ugly hack to make fsspec's arrow implementation work in python 3.7
+    # since arrow requires files to be seekable, and the default fsspec
+    # open(..., seekable) parameter is False
+    # See https://github.com/fsspec/filesystem_spec/pull/1186
+    ArrowFSWrapper._open.__wrapped__.__defaults__ = ("rb", None, True)
+
+
+def walk_match(
+    fs: FileSystem,
+    root: str,
+    file_pattern: str,
+    recursive: bool = True,
+) -> list:
+    if fsspec_version >= "2023.10.0":
+        # Version fixes fsspec glob https://github.com/fsspec/filesystem_spec/pull/1329
+        glob_str = os.path.join(root, "**" if recursive else "", file_pattern)
+        return fs.glob(glob_str)
+    return [
+        os.path.join(dirpath, f)
+        for dirpath, dirnames, files in fs.walk(
+            root,
+            maxdepth=None if recursive else 1,
+        )
+        for f in fnmatch.filter(files, file_pattern)
+    ]
 
 
 def normalize_fs_path(

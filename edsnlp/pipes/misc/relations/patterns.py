ed7adf9dad62b1fcffbc0addbab51610966ad4c8
pidoux7 <faure.guillaume75@gmail.com>
2024-07-03 15:35:26 +0200

    fix: Fix import statement in relations module 'eds.relations' Added: rule based model for relation prediction using proximity and sentences

--- edsnlp/pipes/misc/relations/patterns.py
+++ edsnlp/pipes/misc/relations/patterns.py
@@ -1,17 +1,17 @@
 scheme = [
-                {
-                    "subject": [{"label": "Chemical_and_drugs", "attr": {"Tech": [None]}}],
-                    "object": [
-                        {
-                            "label": "Temporal",
-                            "attr": {"AttTemp": [None, "Duration", "Date", "Time"]},
-                        },
-                        {
-                            "label": "Chemical_and_drugs",
-                            "attr": {"Tech": ["dosage", "route", "strength", "form"]},
-                        },
-                    ],
-                    "type": "Depend",
-                    "inv_type": "inv_Depend",
-                },
-            ]
+    {
+        "source": [{"label": "Chemical_and_drugs", "attr": {"Tech": [None]}}],
+        "target": [
+            {
+                "label": "Temporal",
+                "attr": {"AttTemp": [None, "Duration", "Date", "Frequency"]},
+            },
+            {
+                "label": "Chemical_and_drugs",
+                "attr": {"Tech": ["dosage", "route", "strength", "form"]},
+            },
+        ],
+        "type": "Depend",
+        "inv_type": "inv_Depend",
+    },
+]

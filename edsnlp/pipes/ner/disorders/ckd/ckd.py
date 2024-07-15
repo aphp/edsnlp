e5a998d3ec823e34a0902066c5a914ce699e4067
Ariel Cohen <ariel.cohen-ext@aphp.fr>
2024-07-15 12:28:38 +0200

    add citation

--- edsnlp/pipes/ner/disorders/ckd/ckd.py
+++ edsnlp/pipes/ner/disorders/ckd/ckd.py
@@ -1,4 +1,5 @@
 """`eds.ckd` pipeline"""
+
 from typing import Any, Dict, List, Optional, Union
 
 from loguru import logger
@@ -90,9 +91,9 @@ class CKDMatcher(DisorderMatcher):
 
     Authors and citation
     --------------------
-    The `eds.CKD` component was developed by AP-HP's Data Science team with a team of
-    medical experts. A paper describing in details the development of those components
-    is being drafted and will soon be available.
+    The `eds.ckd` component was developed by AP-HP's Data Science team with a
+    team of medical experts, following the insights of the algorithm proposed
+    by [@petitjean_2024].
     """
 
     def __init__(

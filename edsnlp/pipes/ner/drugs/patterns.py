e2fc8051c1a8f2c8adce9a291827615830de21ff
svittoz <137794505+svittoz@users.noreply.github.com>
2024-08-28 18:24:56 +0200

    Update patterns.py

--- edsnlp/pipes/ner/drugs/patterns.py
+++ edsnlp/pipes/ner/drugs/patterns.py
@@ -5,14 +5,6 @@ from edsnlp import BASE_DIR
 
 drugs_file = BASE_DIR / "resources" / "drugs.json"
 
-
-def filter_dict_by_keys(D: Dict[str, List[str]], L: List[str]):
-    filtered_dict = {
-        k: v for k, v in D.items() if any(k.startswith(prefix) for prefix in L)
-    }
-    return filtered_dict
-
-
 def get_patterns(atc: List[str] = None) -> Dict[str, List[str]]:
     with open(drugs_file, "r") as f:
         patterns = json.load(f)

0a39f5b33d414703278b4a7abd837864959cad05
Perceval Wajsbürt <perceval.wajsburt@aphp.fr>
2025-01-21 22:27:17 +0100

    fix: eds.split now keeps doc and span attributes in the sub-documents

--- tests/pipelines/misc/test_split.py
+++ tests/pipelines/misc/test_split.py
@@ -1,3 +1,4 @@
+from spacy.tokens import Doc
 from spacy.tokens.span import Span
 
 import edsnlp
@@ -12,16 +13,33 @@ A third test!"""
     nlp.add_pipe("eds.matcher", config={"terms": {"test": "test"}})
     doc = nlp(txt)
     Span.set_extension("test_dict", default={}, force=True)
+    Doc.set_extension("global_attr", default=None, force=True)
+    Span.set_extension("ent_attr", default=None, force=True)
+    doc._.global_attr = "global"
     doc.ents[0]._.test_dict = {"key": doc.ents[1]}
+    doc.ents[0]._.ent_attr = "ent-0"
     doc.ents[1]._.test_dict = {"key": doc.ents[0]}
     doc.ents[2]._.test_dict = {"key": doc.ents[0]}
+    doc.ents[2]._.ent_attr = "ent-2"
+    doc.spans["section"] = [doc[:]]
+    doc.spans["section"][0]._.ent_attr = "section"
     subdocs = list(eds.split(regex="\n\n")(doc))
+
     assert len(subdocs) == 2
     assert subdocs[0].text == "This is a test. Another test.\n\n"
-    assert subdocs[1].text == "A third test!"
+    assert subdocs[0]._.global_attr == "global"
     assert subdocs[0].ents[0]._.test_dict["key"] == subdocs[0].ents[1]
+    assert subdocs[0].ents[0]._.ent_attr == "ent-0"
     assert subdocs[0].ents[1]._.test_dict["key"] == subdocs[0].ents[0]
+    assert subdocs[0].spans["section"][0].text == "This is a test. Another test.\n\n"
+    assert subdocs[0].spans["section"][0]._.ent_attr == "section"
+
+    assert subdocs[1].text == "A third test!"
+    assert subdocs[1]._.global_attr == "global"
     assert subdocs[1].ents[0]._.test_dict == {}
+    assert subdocs[1].ents[0]._.ent_attr == "ent-2"
+    assert subdocs[1].spans["section"][0].text == "A third test!"
+    assert subdocs[1].spans["section"][0]._.ent_attr == "section"
 
 
 def test_filter():

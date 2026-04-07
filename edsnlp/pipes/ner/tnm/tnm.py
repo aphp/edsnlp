6c129c1c14b94e5a3e8886b9030117c6380e7cf3
Lucas Dedieu <45566495+LucasDedieu@users.noreply.github.com>
2026-04-07 16:29:17 +0200

    fix: TNM ban word filter

--- edsnlp/pipes/ner/tnm/tnm.py
+++ edsnlp/pipes/ner/tnm/tnm.py
@@ -126,13 +126,14 @@ class TNMMatcher(BaseNERComponent):
         for span, gd in spans:
             text = span.text
             clean = text.replace(" ", "").replace("\n", "").replace(",", "")
+            if clean.strip().lower() in banned_words:
+                continue
             if (
                 # we keep it if it's longer than 2 chars
                 len(clean) > 2
                 # or shorter but there is no space, and it starts w/ a lowercase letter
                 # to avoid cases like "a  T" or "PT"
                 or (not text[1:2].isspace() and text[0:1].islower())
-                and (clean.strip().lower() not in banned_words)
             ):
                 filtered_spans.append((span, gd))
 
